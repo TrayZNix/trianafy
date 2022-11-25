ce2ce78bd831f3cd710f964f8cc71c054de21130
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-11-25 19:55:06 +0100

    Arreglados return (Obj artista -> string nombreArtista)

--- src/main/java/com/salesianostriana/dam/trianafy/mappers/PlaylistMapper.java
+++ src/main/java/com/salesianostriana/dam/trianafy/mappers/PlaylistMapper.java
@@ -1,9 +1,9 @@
 package com.salesianostriana.dam.trianafy.mappers;
 
-import com.salesianostriana.dam.trianafy.dto.PlaylistDtoIn;
-import com.salesianostriana.dam.trianafy.dto.PlaylistDtoOut;
-import com.salesianostriana.dam.trianafy.dto.PlaylistDtoOutPCreate;
+import com.salesianostriana.dam.trianafy.dto.*;
 import com.salesianostriana.dam.trianafy.model.Playlist;
+import com.salesianostriana.dam.trianafy.model.Song;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -11,6 +11,8 @@ import java.util.List;
 
 @Component
 public class PlaylistMapper {
+    @Autowired
+    private SongMapper songMapper;
     public List<PlaylistDtoOut> toPlaylistListDtoOut(List<Playlist> listaPlaylists){
         List<PlaylistDtoOut> playlistListDto = new ArrayList<PlaylistDtoOut>();
         listaPlaylists.forEach(playlist -> {
@@ -29,5 +31,15 @@ public class PlaylistMapper {
 
     public PlaylistDtoOutPCreate toPlaylistDtoOutPostCreate(Playlist playlist){
         return PlaylistDtoOutPCreate.builder().id(playlist.getId()).name(playlist.getName()).description(playlist.getDescription()).build();
+    }
+    public PlaylistDtoOutPCreateWSongs toPlaylistDtoOutPCreateWSongs(Playlist playlist){
+        List<Song> cancionesPlaylist = playlist.getSongs();
+        List<SongDtoOut> cancionesDtoArtista = new ArrayList<SongDtoOut>();
+        cancionesPlaylist.forEach(cancion -> {
+            cancionesDtoArtista.add(songMapper.toSongOut(cancion));
+
+        });
+        return PlaylistDtoOutPCreateWSongs.builder().id(playlist.getId()).description(playlist.getDescription()).name(playlist.getName()).songs(cancionesDtoArtista).build();
+
     }
 }
