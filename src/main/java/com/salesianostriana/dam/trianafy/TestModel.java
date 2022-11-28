cef15609a6d410f568c08def27aa9ca14f81c59f
Roberto Carvajal <carvajal.verob22@triana.salesianos.edu>
2022-11-28 14:28:04 +0100

    añadidos datos de ejemplo

--- src/main/java/com/salesianostriana/dam/trianafy/TestModel.java
+++ src/main/java/com/salesianostriana/dam/trianafy/TestModel.java
@@ -35,7 +35,15 @@ public class TestModel {
                 .name("Metallica")
                 .build();
 
-        List<Artista> artistaList = List.of(a1, a2, a3);
+        Artista a4 = Artista.builder()
+                .name("Rels B")
+                .build();
+
+        Artista a5 = Artista.builder()
+                .name("Toteking")
+                .build();
+
+        List<Artista> artistaList = List.of(a1, a2, a3, a4, a5);
 
         artistaList.forEach(artistService::add);
 
@@ -95,20 +103,57 @@ public class TestModel {
                 .title("Nothing Else Matters")
                 .build();
 
+        Song s1a4 = Song.builder()
+                .album("A mi")
+                .artist(a4)
+                .year("2019")
+                .title("A mi")
+                .build();
+
+        Song s2a4 = Song.builder()
+                .album("La última canción")
+                .artist(a4)
+                .year("2020")
+                .title("La isla EP")
+                .build();
+
+        Song s1a5 = Song.builder()
+                .album("RapSinCorte")
+                .artist(a5)
+                .year("2021")
+                .title("Rap sin corte L")
+                .build();
+
+        Song s2a5 = Song.builder()
+                .album("Dios bendiga este EP")
+                .artist(a5)
+                .year("2022")
+                .title("Man on the moon")
+                .build();
+
         List<Song> songList = List.of(
                 s1a1, s2a1, s3a1,
                 s1a2, s2a2,
-                s1a3, s2a3, s3a3
+                s1a3, s2a3, s3a3,
+                s1a4, s2a4,
+                s1a5, s2a5
         );
 
         songList.forEach(songService::add);
+
 
         Playlist p1 = Playlist.builder()
                 .name("Random")
                 .description("Una lista muy loca")
                 .build();
 
+        Playlist p2 = Playlist.builder()
+                .name("Rap y latino")
+                .description("La musica de los jóvenes :)")
+                .build();
+
         playlistService.add(p1);
+        playlistService.add(p2);
 
         p1.addSong(s1a3);
         p1.addSong(s2a2);
@@ -117,7 +162,16 @@ public class TestModel {
 
         playlistService.edit(p1);
 
+        p2.addSong(s1a4);
+        p2.addSong(s2a5);
+        p2.addSong(s1a5);
+        p2.addSong(s2a4);
+
+        playlistService.edit(p2);
+
         System.out.println(p1);
+        System.out.println(p2);
+
 
 
 
